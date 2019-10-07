10ab71af44c9ed0b0218a3002fd4b66b6bf79a3f
vfghbvfhdscde <50321788+vfghbvfhdscde@users.noreply.github.com>
2019-10-07 04:04:08 +0300

    Add files via upload

---
+++ dndn.js
@@ -0,0 +1,24 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+console.log("Welcome !");
+
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`
+**
+عند خروج العضو من السيرفر
+**`)
+}).catch(console.error)
+
+})
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`
+**
+عند دخول عضو على السيرفر
+**`)
+}).catch(console.error)
+})
+client.login('توكن حساب النشر');
